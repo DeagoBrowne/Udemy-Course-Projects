feeed3e3cfdc87f9f2d9045eb9e894b9df03bc83
Deago <brownedeago@gmail.com>
2022-11-12 04:26:18 +0000

    added event listeners for all form sections

--- script.js
+++ script.js
@@ -10,6 +10,13 @@ const showError = (input, message) => {
   const formControl = input.parentElement;
   formControl.className = 'form-control error';
   const small = formControl.querySelector('small');
+  small.innerText = message;
+}
+
+// Show Success outline
+const showSuccess = (input, message) => {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success';
 }
 
 
@@ -17,9 +24,23 @@ const showError = (input, message) => {
 form.addEventListener('submit', (event) => {
   event.preventDefault();
 
-  console.log(username.value);
   if (username.value === '') {
     showError(username, 'Username is required');
+  } else {
+    showSuccess(username);
+  }
+  if (email.value === '') {
+    showError(email, 'Email is required');
+  } else {
+    showSuccess(username);
+  }
+  if (password.value === '') {
+    showError(password, 'Password is required');
+  } else {
+    showSuccess(username);
+  }
+  if (password2.value === '') {
+    showError(password2, 'Please confirm password');
   } else {
     showSuccess(username);
   }
